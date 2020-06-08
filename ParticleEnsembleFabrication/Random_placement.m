d1fb3a2efcce8acf8132dca79edfd0b70a6fbf2a
Lars Kool <lars.kool@home.nl>
2020-06-08 13:24:11 +0200

    add start of particle placement script

---
+++ ParticleEnsembleFabrication/Random_placement.m
@@ -0,0 +1,12 @@
+%all units used are in mm, as the stage requires input in mm (and for the
+%calculations it doesnt matter).
+
+ChannelW = 5; %Width of the microfluidic channel to be filledin mm
+ChannelL = 5; %Length of the microfluidic channel to be filled in mm
+
+Rsmall = 0.05; %Radius of the small particles in mm
+Rlarge = 0.75; %Radius of the large particles in mm
+
+
+
+
