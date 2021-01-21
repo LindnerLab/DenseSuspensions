b2f6c53f259ddb03a39dd873069334c83d4291c0
Lars-Kool <lars.kool@home.nl>
2021-01-21 17:33:30 +0100

    Introduction of particle interactions

--- ParticleEnsembleFabrication/Lubachevski-Stillinger/Random_Placement.py
+++ ParticleEnsembleFabrication/Lubachevski-Stillinger/Random_Placement.py
@@ -15,7 +15,9 @@ from distance_to_neighbors import distance_to_neighbors
 from neighboring_particles import neighboring_particles
 
 class particles:
-    def __init__(self, xmax, ymax, R, N):
+    def __init__(self, xmax, ymax, R, N, k_wall, k_particle):
+        self.xmax = xmax
+        self.ymax = ymax
         self.pos = np.array([np.random.rand(np.sum(N))*xmax,
                              np.random.rand(np.sum(N))*ymax]).transpose()
         self.r = np.concatenate([np.ones([Nparticles[i], ])*Rparticles[i] for i in range(len(Nparticles))], axis=0)
@@ -27,6 +29,8 @@ class particles:
         self.a = np.zeros([np.sum(N),2])
         self.a_dt = np.zeros([np.sum(N),2])
         self.Ntot = np.sum(N)
+        self.k_wall = k_wall
+        self.k_particle = k_particle
         
     # Generate a new list of neighbors for each particle. This list does not
     # have to be updated every cycle (frequency depends on the speed and
@@ -40,14 +44,39 @@ class particles:
         
     def update_force(self):
         for i in range(self.Ntot):
-            forces = []
+            forces = np.array([0, 0]).reshape([1,2])
             xy_particle = self.pos[i,:]
             xy_neighbors = self.pos[self.neighbors[i]]
             d_neighbors = distance_to_neighbors(xy_particle, xy_neighbors)
             
+            # Interparticle forces
+            r1 = self.r[i]
+            r2 = self.r[self.neighbors[i]]
             for j in range(len(self.neighbors[i])):
-                if d_neighbors > (self.r[i] + self.r[self.neighbor[i][j]]):
-                    forces.append()
+                if d_neighbors[j] < (self.r[i] + self.r[self.neighbors[i][j]]):
+                    dpos = self.pos[i] - self.pos[self.neighbors[i][j]]
+                    Fmag = (d_neighbors[j] - r1 - r2[j]) * self.k_particle
+                    F = np.array([(dpos[0]/d_neighbors[j]) * Fmag,
+                                  (dpos[1]/d_neighbors[j]) * Fmag]).reshape([1,2])
+                    forces = np.concatenate([forces, F], axis=0)
+            
+            # Particle-wall forces
+            if xy_particle[0] < 0:
+                F = np.array([xy_particle[0] * self.k_wall, 0]).reshape([1,2])
+                forces = np.concatenate([forces, F], axis=0)
+            elif xy_particle[0] > self.xmax:
+                F = np.array([(self.xmax - xy_particle[0]) * self.k_wall, 0]).reshape([1,2])  
+                forces = np.concatenate([forces, F], axis=0)
+            
+            if xy_particle[1] < 0:
+                F = np.array([0, xy_particle[1] * self.k_wall]).reshape([1,2])
+                forces = np.concatenate([forces, F], axis=0)
+            elif xy_particle[1] > self.ymax:
+                F = np.array([0, (self.ymax - xy_particle[1]) * self.k_wall]).reshape([1,2])
+                forces = np.concatenate([forces, F], axis=0)
+        
+            self.forces[i] = forces
+            self.Fsum[i,:] = np.sum(forces, axis=0)
 
     def update_acceleration(self):
         self.a = self.a_dt
@@ -64,8 +93,18 @@ if __name__ == '__main__':
     length = 25
     width = 5
     cutoff = 0.3
-    dt = 0.001
+    dt = 0.01
+    k_particle = 1
+    k_wall = 100
     
-    packing = particles(length, width, Rparticles, Nparticles)
+    packing = particles(length, width, Rparticles, Nparticles, k_wall, k_particle)
     packing.update_neighbors(cutoff)
-    packing.update_force()
+    test = 0
+    
+    while test < 10000:
+        packing.update_position(dt)
+        packing.update_force()
+        if np.sum(packing.Fsum) > 0:
+            test = 10001
+        test = test + 1
+        
