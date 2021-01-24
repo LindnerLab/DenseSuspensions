909330e0eecf3f2303884e07d7ce77745f33f598
Lars Kool <lars.kool@home.nl>
2021-01-24 19:09:43 +0100

    Added Ekin, Epot and Etot calculations

--- ParticleEnsembleFabrication/Lubachevski-Stillinger/Random_Placement.py
+++ ParticleEnsembleFabrication/Lubachevski-Stillinger/Random_Placement.py
@@ -25,7 +25,7 @@ class particles:
                              np.random.rand(np.sum(N)) * ymax]).transpose()
         self.r = np.concatenate([np.ones([Nparticles[i], ]) * Rparticles[i] for i in range(len(Nparticles))], axis=0)
         # self.v = np.random.rand(np.sum(N), 2) * 2 - 1
-        self.v = initial_velocity(N)
+        self.v = np.zeros([np.sum(N), 2])
         self.m = np.ones([np.sum(N), ])
         self.neighbors = []
         self.forces = [[] for i in range(np.sum(N))]
@@ -35,11 +35,13 @@ class particles:
         self.Ntot = np.sum(N)
         self.k_wall = k_wall
         self.k_particle = k_particle
-        
-    def initial_velocity(self, N):
-        phi = np.random.rand(np.sum(N)) * 2 * np.pi
-        v = np.array([np.cos(phi), np.sin(phi)])
-        pass
+        self.Epot = 0
+        self.Ekin = 0
+        self.Etot = 0
+
+    def initial_velocity(self):
+        phi = np.random.rand(self.Ntot) * 2 * np.pi
+        self.v = np.array([np.cos(phi), np.sin(phi)]).transpose()
 
     # Generate a new list of neighbors for each particle. This list does not
     # have to be updated every cycle (frequency depends on the speed and
@@ -53,7 +55,7 @@ class particles:
 
     def update_force(self):
         for i in range(self.Ntot):
-            forces = np.array([0, 0]).reshape([1, 2])
+            forces = np.array([0, 0, 1]).reshape([1, 3])
             xy_particle = self.pos[i, :]
             xy_neighbors = self.pos[self.neighbors[i]]
             d_neighbors = distance_to_neighbors(xy_particle, xy_neighbors)
@@ -66,26 +68,31 @@ class particles:
                     dpos = self.pos[i] - self.pos[self.neighbors[i][j]]
                     Fmag = (d_neighbors[j] - r1 - r2[j]) * self.k_particle
                     F = -np.array([(dpos[0] / d_neighbors[j]) * Fmag,
-                                  (dpos[1] / d_neighbors[j]) * Fmag]).reshape([1, 2])
+                                   (dpos[1] / d_neighbors[j]) * Fmag,
+                                   self.k_particle]).reshape([1, 3])
                     forces = np.concatenate([forces, F], axis=0)
 
             # Particle-wall forces
             if xy_particle[0] < 0:
-                F = -np.array([xy_particle[0] * self.k_wall, 0]).reshape([1, 2])
+                F = -np.array([xy_particle[0] * self.k_wall, 0,
+                               self.k_wall]).reshape([1, 3])
                 forces = np.concatenate([forces, F], axis=0)
             elif xy_particle[0] > self.xmax:
-                F = np.array([(self.xmax - xy_particle[0]) * self.k_wall, 0]).reshape([1, 2])
+                F = np.array([(self.xmax - xy_particle[0]) * self.k_wall, 0,
+                              self.k_wall]).reshape([1, 3])
                 forces = np.concatenate([forces, F], axis=0)
 
             if xy_particle[1] < 0:
-                F = -np.array([0, xy_particle[1] * self.k_wall]).reshape([1, 2])
+                F = -np.array([0, xy_particle[1] * self.k_wall,
+                               self.k_wall]).reshape([1, 3])
                 forces = np.concatenate([forces, F], axis=0)
             elif xy_particle[1] > self.ymax:
-                F = np.array([0, (self.ymax - xy_particle[1]) * self.k_wall]).reshape([1, 2])
+                F = np.array([0, (self.ymax - xy_particle[1]) * self.k_wall,
+                              self.k_wall]).reshape([1, 3])
                 forces = np.concatenate([forces, F], axis=0)
 
             self.forces[i] = forces
-            self.Fsum[i, :] = np.sum(forces, axis=0)
+            self.Fsum[i, :] = np.sum(forces[:, 0:2], axis=0)
 
     def update_acceleration(self):
         self.a = self.a_dt
@@ -94,34 +101,51 @@ class particles:
     def update_velocity(self, dt):
         self.v = self.v + 0.5 * (self.a + self.a_dt) * dt
 
+    def update_energies(self):
+        self.Ekin = 0.5 * np.sum(self.m[:, None] * self.v ** 2)
+
+        Epot = np.zeros([self.Ntot, ])
+        for i, force in enumerate(self.forces):
+            dx = np.divide(force[:, 0:2], force[:, 2, None])
+            Epot[i] = 0.5 * np.sum(force[:, 2] * dx[:, 0:1] ** 2)
+        self.Epot = np.sum(Epot)
+        self.Etot = self.Ekin + self.Epot
+
 
 if __name__ == '__main__':
-    Nparticles = [10]
-    Rparticles = [0.03]
+    Nparticles = [1500, 1000]
+    Rparticles = [0.03, 0.045]
     Ncycles = 100000
-    length = 1
-    width = 1
-    cutoff = 0.3
+    length = 30
+    width = 5
+    cutoff = 0.03
     dt = 0.0001
-    k_particle = 100000
-    k_wall = 10000000
+    k_particle = 10000
+    k_wall = 10000
     T = 10
 
-    packing = particles(length, width, Rparticles, Nparticles, k_wall, k_particle, T)
-    packing.update_neighbors(cutoff)
-
-    trajectory = np.zeros((Ncycles, 10, 2))
+    trajectory = np.zeros((Ncycles, np.sum(Nparticles), 2))
     Ekin = np.array([])
     Epot = np.array([])
     Etot = np.array([])
     i = 0
 
+    packing = particles(length, width, Rparticles, Nparticles, k_wall, k_particle, T)
+    packing.initial_velocity()
+    packing.update_neighbors(cutoff)
+
     while i < Ncycles:
-        Ekin = np.concatenate([Ekin, [0.5 * np.sum(packing.v ** 2)]])
+        if i % 1000 == 0:
+            packing.update_neighbors(cutoff)
+            print(str(i*100/Ncycles) + '% done')
         packing.update_position(dt)
         packing.update_force()
         packing.update_acceleration()
         packing.update_velocity(dt)
+        packing.update_energies()
+        Ekin = np.concatenate([Ekin, [packing.Ekin]])
+        Epot = np.concatenate([Epot, [packing.Epot]])
+        Etot = np.concatenate([Etot, [packing.Etot]])
         trajectory[i, :, :] = packing.pos
         F = packing.forces
         Fsum = packing.Fsum
@@ -131,7 +155,7 @@ if __name__ == '__main__':
 
     plt.figure(dpi=500)
     ax = plt.subplot(111)
-    for i in range(10):
+    for i in range(np.sum(Nparticles)):
         ax.plot(trajectory[:, i, 0], trajectory[:, i, 1])
-    plt.xlim([0, 1])
-    plt.ylim([0, 1])
+    plt.xlim([-0.1*length, 1.1*length])
+    plt.ylim([-0.1*width, 1.1*width])
