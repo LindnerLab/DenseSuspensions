dc97a34649cb25fe2e3c5f8c5c0c06c2df65a338
Lars Kool <lars.kool@home.nl>
2021-01-24 10:49:56 +0100

    Changed code to match PEP8 conventions

--- ParticleEnsembleFabrication/Lubachevski-Stillinger/Random_Placement.py
+++ ParticleEnsembleFabrication/Lubachevski-Stillinger/Random_Placement.py
@@ -14,24 +14,25 @@ import pandas as pd
 from distance_to_neighbors import distance_to_neighbors
 from neighboring_particles import neighboring_particles
 
+
 class particles:
     def __init__(self, xmax, ymax, R, N, k_wall, k_particle):
         self.xmax = xmax
         self.ymax = ymax
-        self.pos = np.array([np.random.rand(np.sum(N))*xmax,
-                             np.random.rand(np.sum(N))*ymax]).transpose()
-        self.r = np.concatenate([np.ones([Nparticles[i], ])*Rparticles[i] for i in range(len(Nparticles))], axis=0)
-        self.v = np.random.rand(np.sum(N),2)*2-1
-        self.m = np.ones([np.sum(N),])
+        self.pos = np.array([np.random.rand(np.sum(N)) * xmax,
+                             np.random.rand(np.sum(N)) * ymax]).transpose()
+        self.r = np.concatenate([np.ones([Nparticles[i], ]) * Rparticles[i] for i in range(len(Nparticles))], axis=0)
+        self.v = np.random.rand(np.sum(N), 2) * 2 - 1
+        self.m = np.ones([np.sum(N), ])
         self.neighbors = []
         self.forces = [[] for i in range(np.sum(N))]
-        self.Fsum = np.zeros([np.sum(N),2])
-        self.a = np.zeros([np.sum(N),2])
-        self.a_dt = np.zeros([np.sum(N),2])
+        self.Fsum = np.zeros([np.sum(N), 2])
+        self.a = np.zeros([np.sum(N), 2])
+        self.a_dt = np.zeros([np.sum(N), 2])
         self.Ntot = np.sum(N)
         self.k_wall = k_wall
         self.k_particle = k_particle
-        
+
     # Generate a new list of neighbors for each particle. This list does not
     # have to be updated every cycle (frequency depends on the speed and
     # average interparticle distance)
@@ -39,16 +40,16 @@ class particles:
         self.neighbors = neighboring_particles(self.pos, cutoff)
 
     def update_position(self, dt):
-        self.pos = np.array([self.pos[:,0] + self.v[:,0]*dt + 0.5*self.a[:,0]*(dt**2),
-                             self.pos[:,1] + self.v[:,1]*dt + 0.5*self.a[:,1]*(dt**2)]).transpose()
-        
+        self.pos = np.array([self.pos[:, 0] + self.v[:, 0] * dt + 0.5 * self.a[:, 0] * (dt ** 2),
+                             self.pos[:, 1] + self.v[:, 1] * dt + 0.5 * self.a[:, 1] * (dt ** 2)]).transpose()
+
     def update_force(self):
         for i in range(self.Ntot):
-            forces = np.array([0, 0]).reshape([1,2])
-            xy_particle = self.pos[i,:]
+            forces = np.array([0, 0]).reshape([1, 2])
+            xy_particle = self.pos[i, :]
             xy_neighbors = self.pos[self.neighbors[i]]
             d_neighbors = distance_to_neighbors(xy_particle, xy_neighbors)
-            
+
             # Interparticle forces
             r1 = self.r[i]
             r2 = self.r[self.neighbors[i]]
@@ -56,35 +57,34 @@ class particles:
                 if d_neighbors[j] < (self.r[i] + self.r[self.neighbors[i][j]]):
                     dpos = self.pos[i] - self.pos[self.neighbors[i][j]]
                     Fmag = (d_neighbors[j] - r1 - r2[j]) * self.k_particle
-                    F = np.array([(dpos[0]/d_neighbors[j]) * Fmag,
-                                  (dpos[1]/d_neighbors[j]) * Fmag]).reshape([1,2])
+                    F = np.array([(dpos[0] / d_neighbors[j]) * Fmag,
+                                  (dpos[1] / d_neighbors[j]) * Fmag]).reshape([1, 2])
                     forces = np.concatenate([forces, F], axis=0)
-            
+
             # Particle-wall forces
             if xy_particle[0] < 0:
-                F = np.array([xy_particle[0] * self.k_wall, 0]).reshape([1,2])
+                F = np.array([xy_particle[0] * self.k_wall, 0]).reshape([1, 2])
                 forces = np.concatenate([forces, F], axis=0)
             elif xy_particle[0] > self.xmax:
-                F = np.array([(self.xmax - xy_particle[0]) * self.k_wall, 0]).reshape([1,2])  
+                F = np.array([(self.xmax - xy_particle[0]) * self.k_wall, 0]).reshape([1, 2])
                 forces = np.concatenate([forces, F], axis=0)
-            
+
             if xy_particle[1] < 0:
-                F = np.array([0, xy_particle[1] * self.k_wall]).reshape([1,2])
+                F = np.array([0, xy_particle[1] * self.k_wall]).reshape([1, 2])
                 forces = np.concatenate([forces, F], axis=0)
             elif xy_particle[1] > self.ymax:
-                F = np.array([0, (self.ymax - xy_particle[1]) * self.k_wall]).reshape([1,2])
+                F = np.array([0, (self.ymax - xy_particle[1]) * self.k_wall]).reshape([1, 2])
                 forces = np.concatenate([forces, F], axis=0)
-        
+
             self.forces[i] = forces
-            self.Fsum[i,:] = np.sum(forces, axis=0)
+            self.Fsum[i, :] = np.sum(forces, axis=0)
 
     def update_acceleration(self):
         self.a = self.a_dt
-        self.a_dt = self.force/self.m
-    
+        self.a_dt = self.force / self.m
+
     def update_velocity(self):
-        self.v = self.v + 0.5*(self.a + self.a_dt)*dt
-    
+        self.v = self.v + 0.5 * (self.a + self.a_dt) * dt
 
 
 if __name__ == '__main__':
@@ -96,15 +96,14 @@ if __name__ == '__main__':
     dt = 0.01
     k_particle = 1
     k_wall = 100
-    
+
     packing = particles(length, width, Rparticles, Nparticles, k_wall, k_particle)
     packing.update_neighbors(cutoff)
     test = 0
-    
+
     while test < 10000:
         packing.update_position(dt)
         packing.update_force()
         if np.sum(packing.Fsum) > 0:
             test = 10001
         test = test + 1
-        
--- ParticleTracking/Python/Strain_functions.py
+++ ParticleTracking/Python/Strain_functions.py
@@ -12,53 +12,58 @@ import math as m
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-def calc_strain(tracked, Cauchy=False, Hencky=False, direction='x', x_max=np.nan):
+
+def calc_strain(tracked, Cauchy=False, Hencky=False,
+                direction='x', x_max=np.nan):
+
     nParticles = len(tracked.particle.unique())
     nFrames = len(tracked.frame.unique())
-    
+
     # Set the expected sorting
     if all(tracked[:nParticles].particle == tracked.particle.unique()):
-        tracked = tracked.sort_values(by=['frame','particle'],
+        tracked = tracked.sort_values(by=['frame', 'particle'],
                                       ignore_index=True)
-    elif not (all(tracked[:nParticles].frame == 0) and
-              all(tracked.frame[list(range(0,nFrames*nParticles,nParticles))]
-                  == tracked.frame.unique())):
-        tracked = tracked.sort_values(by=['frame','particle'],
+    elif not (all(tracked[:nParticles].frame == 0) and all(tracked.frame[list(range(0, nFrames * nParticles, nParticles))] == tracked.frame.unique())):
+        tracked = tracked.sort_values(by=['frame', 'particle'],
                                       ignore_index=True)
     if x_max == np.nan:
         x_max = [np.max(tracked[direction][tracked.particle == i])
                  for i in range(nParticles)]
     max_length = np.tile(x_max, nFrames)
     current_length = np.array(tracked[direction])
-    
+
     if Cauchy:
-        Cauchy_strain = -(current_length - max_length)/max_length
+        Cauchy_strain = -(current_length - max_length) / max_length
         tracked['Cauchy_strain'] = Cauchy_strain
     if Hencky:
-        Hencky_strain = -np.log(current_length/max_length)
+        Hencky_strain = -np.log(current_length / max_length)
         tracked['Hencky_strain'] = Hencky_strain
     return tracked
 
-def calc_strain_rate(tracked, Cauchy=False, Hencky=False, dt=1, framerate=1, direction='x'): 
+
+def calc_strain_rate(tracked, Cauchy=False, Hencky=False,
+                     dt=1, framerate=1, direction='x'):
+
     if (Cauchy and 'Cauchy_strain' not in tracked.columns) or (Hencky and 'Hencky_strain' not in tracked.columns):
         print('''Required strain not found. Therefore, the strain is \n calculated first. The calculation will take a bit longer.''')
         tracked = calc_strain(tracked, Cauchy=Cauchy, Hencky=Hencky)
-    
+
     nParticles = len(tracked.particle.unique())
     if Cauchy:
-        Cauchy_strain_min_dt = np.array(tracked.Cauchy_strain[:-2*dt*nParticles])
-        Cauchy_strain_plus_dt = np.array(tracked.Cauchy_strain[2*dt*nParticles:])
-        Cauchy_strain_rate = ((Cauchy_strain_plus_dt - Cauchy_strain_min_dt)*framerate)/(2*dt)
-        Cauchy_strain_rate = np.pad(Cauchy_strain_rate, dt*nParticles, mode='constant', constant_values=np.nan)
+        Cauchy_strain_min_dt = np.array(tracked.Cauchy_strain[:-2 * dt * nParticles])
+        Cauchy_strain_plus_dt = np.array(tracked.Cauchy_strain[2 * dt * nParticles:])
+        Cauchy_strain_rate = ((Cauchy_strain_plus_dt - Cauchy_strain_min_dt) * framerate) / (2 * dt)
+        Cauchy_strain_rate = np.pad(Cauchy_strain_rate, dt * nParticles, mode='constant', constant_values=np.nan)
         tracked['Cauchy_strain_rate'] = Cauchy_strain_rate
-        
+
     if Hencky:
-        Hencky_strain_min_dt = np.array(tracked.Hencky_strain[:-2*dt*nParticles])
-        Hencky_strain_plus_dt = np.array(tracked.Hencky_strain[2*dt*nParticles:])
-        Hencky_strain_rate = ((Hencky_strain_plus_dt - Hencky_strain_min_dt)*framerate)/(2*dt)
-        Hencky_strain_rate = np.pad(Hencky_strain_rate, dt*nParticles, mode='constant', constant_values=np.nan)
+        Hencky_strain_min_dt = np.array(tracked.Hencky_strain[:-2 * dt * nParticles])
+        Hencky_strain_plus_dt = np.array(tracked.Hencky_strain[2 * dt * nParticles:])
+        Hencky_strain_rate = ((Hencky_strain_plus_dt - Hencky_strain_min_dt) * framerate) / (2 * dt)
+        Hencky_strain_rate = np.pad(Hencky_strain_rate, dt * nParticles, mode='constant', constant_values=np.nan)
         tracked['Hencky_strain_rate'] = Hencky_strain_rate
     return tracked
+
 
 if __name__ == '__main__':
     tracked = pd.read_pickle(r'E:\Lars\Oscillatory Compression\20201103 Creep\Decompression_125Start_26End_25Back\Preprocessed\V1\tracked.pkl')
